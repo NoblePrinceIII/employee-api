bfdf51b27cf69515f27bebd031f646865291ec6d
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-02-06 08:23:23 -0800

    added more routes and error messages in controller

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -17,9 +17,46 @@ router.post('/', (req, res) => {
 
 function insertRecord(req, res) {
     var employee = new Employee();
-    employee.fullname = req.body.fullName;
-
+    employee.fullName = req.body.fullName;
+    employee.email = req.body.email;
+    employee.phone = req.body.phone;
+    employee.city = req.body.city;
+    employee.save((err, doc) => {
+        if (!err)
+            res.redirect('employee/list');
+        else {
+            if (err.name == 'ValidationError') {
+                handleValidationError(err, req.body);
+                res.render("employee/addOrEdit", {
+                    viewTitle: "Insert Employee",
+                    employee: req.body
+                });
+            } else
+                console.log('Error during record insertion : ' + err);
+        }
+    });
 }
 
+router.get('/list', (req, res) => {
+    res.json('from list');
+});
+
+
+
+
+function handleValidationError(err, body) {
+    for (field in err.errors) {
+        switch (err.errors[field].path) {
+            case 'fullName':
+                body['fullNameError'] = err.errors[field].message;
+                break;
+            case 'email':
+                body['emailError'] = err.errors[field].message;
+                break;
+            default:
+                break;
+        }
+    }
+}
 
 module.exports = router;
